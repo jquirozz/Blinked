c4e988f93125b9d867280254fe02639c2c6f29bc
Joseph <jq84144@gmail.com>
2024-02-24 02:32:36 -0500

    NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom'
+import './NotFound.scss'
+
 function NotFound () {
-  return <div>NotFound</div>
+  return (
+    <div className='NotFound'>
+      <header>
+        <img src='/logo/big-pink.png' alt='' />
+        <h2>OOPS!</h2>
+        <h3>Page not found</h3>
+      </header>
+      <Link to='/'>
+        <h2>Go back</h2>
+      </Link>
+    </div>
+  )
 }
 
 export default NotFound
