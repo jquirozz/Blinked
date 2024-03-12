bbaac076574c65eaca1e77eecf1f65587eaeb6f8
Joseph <jq84144@gmail.com>
2024-03-12 15:29:46 -0500

    New: Not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,25 @@
+import { Link } from 'react-router-dom'
+
 import './NotFound.scss'
+import { GoAlert } from 'react-icons/go'
 
 function NotFound () {
-  return <div className='NotFound'>Not Found</div>
+  return (
+    <div className='NotFound'>
+      <header>
+        <GoAlert />
+      </header>
+      <footer>
+        <section>
+          <h2>NOT FOUND</h2>
+          <h3>or no data provided</h3>
+        </section>
+        <Link to='/'>
+          <h4>GO HOME</h4>
+        </Link>
+      </footer>
+    </div>
+  )
 }
 
 export default NotFound
